2c749be8e044f603a688a0df49b33543cef7aa54
evelynn333 <evelyncn333@gmail.com>
2026-04-17 20:53:52 +0200

    cambio estilos

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,50 +2,74 @@ import { Box, Typography } from "@mui/material";
 
 export const Hero = () => {
     return (
-        <Box sx={{ textAlign: "center" }}>
-            {/* Nombres */}
-            <Typography
+        <Box
+            sx={{
+                position: "relative",
+                textAlign: "center",
+            }}
+        >
+            {/* 🕊️ Marca de agua */}
+            <Box
+                component="img"
+                src="./icons/couple.png"
+                alt="Pareja"
                 sx={{
-                    fontFamily: "Playfair Display",
-                    fontSize: { xs: "2.5rem", md: "3.5rem" },
-                    lineHeight: 1.2,
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: { xs: "400px", md: "500px" },
+                    opacity: 0.05,
+                    zIndex: 0,
+                    pointerEvents: "none",
                 }}
-            >
-                Maricarmen
-            </Typography>
+            />
 
-            <Typography
-                sx={{
-                    fontFamily: "Playfair Display",
-                    fontSize: "2rem",
-                    my: 1,
-                    opacity: 0.7,
-                }}
-            >
-                &
-            </Typography>
+            {/* Contenido */}
+            <Box sx={{ position: "relative", zIndex: 1 }}>
+                {/* Nombres */}
+                <Typography
+                    sx={{
+                        fontFamily: "Playfair Display",
+                        fontSize: { xs: "2.5rem", md: "3.5rem" },
+                        lineHeight: 1.2,
+                    }}
+                >
+                    Maricarmen
+                </Typography>
 
-            <Typography
-                sx={{
-                    fontFamily: "Playfair Display",
-                    fontSize: { xs: "2.5rem", md: "3.5rem" },
-                }}
-            >
-                Juanfran
-            </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: "Playfair Display",
+                        fontSize: "2rem",
+                        my: 1,
+                        opacity: 0.7,
+                    }}
+                >
+                    &
+                </Typography>
 
-            {/* Fecha */}
-            <Typography
-                sx={{
-                    mt: 3,
-                    fontSize: "0.8rem",
-                    letterSpacing: "2px",
-                    color: "text.secondary",
-                }}
-            >
-                5 SEPTIEMBRE 2026
-            </Typography>
-            
+                <Typography
+                    sx={{
+                        fontFamily: "Playfair Display",
+                        fontSize: { xs: "2.5rem", md: "3.5rem" },
+                    }}
+                >
+                    Juanfran
+                </Typography>
+
+                {/* Fecha */}
+                <Typography
+                    sx={{
+                        mt: 3,
+                        fontSize: "0.8rem",
+                        letterSpacing: "2px",
+                        color: "text.secondary",
+                    }}
+                >
+                    5 SEPTIEMBRE 2026
+                </Typography>
+            </Box>
         </Box>
     );
 };
